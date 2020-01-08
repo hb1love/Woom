0cfebfb1f54021953a18b4d2cca37512679e4d3f
Kim Heebeom <heebuma@gmail.com>
2020-01-09 00:43:18 +0900

    make project

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.0'
+workspace 'Um'
+
+inhibit_all_warnings!
+use_frameworks!
+
+
+target 'Um' do
+  project 'Um/Um'
+end
---
+++ Um/Um/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Um
+//
+//  Created by NHNEnt on 2020/01/09.
+//  Copyright © 2020 depromeet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
